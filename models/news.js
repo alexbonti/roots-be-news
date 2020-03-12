e96e8b7c4a132eef4028dec86f9d2092a5417b3c
Anirudhm9 <anirudh.m0009@gmail.com>
2020-03-13 09:21:04 +1100

    bug fix

--- models/news.js
+++ models/news.js
@@ -4,15 +4,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Config = require('../config');
+var image = 'https://s3.au-syd.cloud-object-storage.appdomain.cloud/ipan-v2-bucket/image/profilePicture/original/Profile_p6ShcttnsmkW.png';
 
 var news = new Schema({
   title: { type: String, trim: true },
   content: { type: String, trim: true },
-  imageURL: { type: String },
+  imageURL: { type: String, default: image },
   datePublished: { type: Date, default: Date.now },
   link: { type: String, trim: true },
   category: { type: String, trim: true }
 });
-news.index({ "title": 1, "content": 1, "link": 1, "category": 1 }, { unique: true })
+news.index({ "title": 1, "content": 1, "category": 1 }, { unique: true })
 news.index({ title: "text" })
 module.exports = mongoose.model('news', news);
